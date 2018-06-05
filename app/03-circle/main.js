fa638473fbaccbcbca77d8ea179537910166a2dc
Michel PIANCA <michel.pianca@gmail.com>
2018-06-05 09:30:12 +0200

    ok

--- app/03-circle/main.js
+++ app/03-circle/main.js
@@ -1,13 +1,20 @@
-function makeCircle() {
+class circle {
+  constructor() {}
+
+  draw() {
     const div = document.querySelector('div');
     div.innerHTML = `
   <svg height="600" width="600">
   <circle cx="300" cy="300" r="200" stroke="#000" stroke-width="3" fill="transparent" />
 </svg>`;
+  }
 }
 
+
+
 function main() {
-  makeCircle();
+  const c = new circle();
+  c.draw();
 }
 
 main();
